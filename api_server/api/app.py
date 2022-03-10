b62eadbf460a924efe2847578d3e581d164c6a20
akshatananjappa <91104247+akshatananjappa@users.noreply.github.com>
2022-03-10 13:45:37 -0500

    Image display added

--- api_server/api/app.py
+++ api_server/api/app.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from models.Users import Users
+import os
+from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import base64
 #import SQLAlchemy
 
 #db = SQLAlchemy(host="", user="", password="", port="")
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root():
@@ -25,3 +31,26 @@ def login():
         return {"message": "Login successful"}
     else:
         return {"message": "Invalid credentials"}
+
+
+@app.get("/get-images/{event_id}", response_class=HTMLResponse)
+def get_images(request:Request, event_id):
+    try:
+        print(os.getcwd())
+        if event_id:
+            base_dir = 'event_artifacts/interim/'
+            file_path = os.path.join(base_dir, event_id, 'image.png')
+            with open(file_path, 'rb') as file:
+                encoded_bytes = base64.b64encode(file.read()) 
+            print(encoded_bytes)
+            #image_bytes = open(file_path, 'rb').read()
+            #print(image_bytes)
+            return templates.TemplateResponse("index.html", {"request": request, "event_id": event_id, "png_base64": encoded_bytes.decode('utf-8')})
+
+    except Exception as e:
+        print(e)
+
+
+
+
+
---
+++ api_server/api/services/AnalyzeNowcast.py
@@ -0,0 +1,132 @@
+import os
+os.environ["HDF5_USE_FILE_LOCKING"]='FALSE'
+import sys
+sys.path.append('../neurips-sevir/src/')
+import h5py
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import ListedColormap
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+import matplotlib.patches as patches
+import pandas as pd
+
+from display.display import get_cmap
+
+# Load pretrained nowcasting models
+mse_file  = '../neurips-sevir/models/nowcast/mse_model.h5'
+mse_model = tf.keras.models.load_model(mse_file,compile=False,custom_objects={"tf": tf})
+
+style_file = '../neurips-sevir/models/nowcast/style_model.h5'
+style_model = tf.keras.models.load_model(style_file,compile=False,custom_objects={"tf": tf})
+
+mse_style_file = '../neurips-sevir/models/nowcast/mse_and_style.h5'
+mse_style_model = tf.keras.models.load_model(mse_style_file,compile=False,custom_objects={"tf": tf})
+
+gan_file = '../neurips-sevir/models/nowcast/gan_generator.h5'
+gan_model = tf.keras.models.load_model(gan_file,compile=False,custom_objects={"tf": tf})
+
+
+# Load a part of the test dataset
+from readers.nowcast_reader import read_data
+x_test,y_test = read_data('../neurips-sevir/data/interim/nowcast_testing.h5',end=50)
+
+
+## 
+# Functions for plotting results
+##
+
+norm = {'scale':47.54,'shift':33.44}
+hmf_colors = np.array( [
+    [82,82,82], 
+    [252,141,89],
+    [255,255,191],
+    [145,191,219]
+])/255
+
+# Model that implements persistence forecast that just repeasts last frame of input
+class persistence:
+    def predict(self,x_test):
+        return np.tile(x_test[:,:,:,-1:],[1,1,1,12])
+
+def plot_hit_miss_fa(ax,y_true,y_pred,thres):
+    mask = np.zeros_like(y_true)
+    mask[np.logical_and(y_true>=thres,y_pred>=thres)]=4
+    mask[np.logical_and(y_true>=thres,y_pred<thres)]=3
+    mask[np.logical_and(y_true<thres,y_pred>=thres)]=2
+    mask[np.logical_and(y_true<thres,y_pred<thres)]=1
+    cmap=ListedColormap(hmf_colors)
+    ax.imshow(mask,cmap=cmap)
+
+
+def visualize_result(models,x_test,y_test,idx,ax,labels):
+    fs=10
+    cmap_dict = lambda s: {'cmap':get_cmap(s,encoded=True)[0],
+                           'norm':get_cmap(s,encoded=True)[1],
+                           'vmin':get_cmap(s,encoded=True)[2],
+                           'vmax':get_cmap(s,encoded=True)[3]}
+    for i in range(1,13,3):
+        xt = x_test[idx,:,:,i]*norm['scale']+norm['shift']
+        ax[(i-1)//3][0].imshow(xt,**cmap_dict('vil'))
+    ax[0][0].set_title('Inputs',fontsize=fs)
+    
+    pers = persistence().predict(x_test[idx:idx+1])
+    pers = pers*norm['scale']+norm['shift']
+    x_test = x_test[idx:idx+1]
+    y_test = y_test[idx:idx+1]*norm['scale']+norm['shift']
+    y_preds=[]
+    for i,m in enumerate(models):
+        yp = m.predict(x_test)
+        if isinstance(yp,(list,)):
+            yp=yp[0]
+        y_preds.append(yp*norm['scale']+norm['shift'])
+    
+    for i in range(0,12,3):
+        ax[i//3][2].imshow(y_test[0,:,:,i],**cmap_dict('vil'))
+    ax[0][2].set_title('Target',fontsize=fs)
+    
+    # Plot Persistence
+    for i in range(0,12,3):
+        plot_hit_miss_fa(ax[i//3][4],y_test[0,:,:,i],pers[0,:,:,i],74)
+    ax[0][4].set_title('Persistence\nScores',fontsize=fs)
+    
+    for k,m in enumerate(models):
+        for i in range(0,12,3):
+            ax[i//3][5+2*k].imshow(y_preds[k][0,:,:,i],**cmap_dict('vil'))
+            plot_hit_miss_fa(ax[i//3][5+2*k+1],y_test[0,:,:,i],y_preds[k][0,:,:,i],74)
+
+        ax[0][5+2*k].set_title(labels[k],fontsize=fs)
+        ax[0][5+2*k+1].set_title(labels[k]+'\nScores',fontsize=fs)
+        
+    for j in range(len(ax)):
+        for i in range(len(ax[j])):
+            ax[j][i].xaxis.set_ticks([])
+            ax[j][i].yaxis.set_ticks([])
+    for i in range(4):
+        ax[i][1].set_visible(False)
+    for i in range(4):
+        ax[i][3].set_visible(False)
+    ax[0][0].set_ylabel('-45 Minutes')
+    ax[1][0].set_ylabel('-30 Minutes')
+    ax[2][0].set_ylabel('-15 Minutes')
+    ax[3][0].set_ylabel('  0 Minutes')
+    ax[0][2].set_ylabel('+15 Minutes')
+    ax[1][2].set_ylabel('+30 Minutes')
+    ax[2][2].set_ylabel('+45 Minutes')
+    ax[3][2].set_ylabel('+60 Minutes')
+    
+    legend_elements = [Patch(facecolor=hmf_colors[1], edgecolor='k', label='False Alarm'),
+                   Patch(facecolor=hmf_colors[2], edgecolor='k', label='Miss'),
+                   Patch(facecolor=hmf_colors[3], edgecolor='k', label='Hit')]
+    ax[-1][-1].legend(handles=legend_elements, loc='lower right', bbox_to_anchor= (-5.4, -.35), 
+                           ncol=5, borderaxespad=0, frameon=False, fontsize='16')
+    plt.subplots_adjust(hspace=0.05, wspace=0.05)
+    
+
+
+idx=25 # adjust this to pick a case
+fig,ax = plt.subplots(4,13,figsize=(24,8), gridspec_kw={'width_ratios': [1,.2,1,.2,1,1,1,1,1,1,1,1,1]})
+visualize_result([mse_model,style_model,mse_style_model,gan_model],x_test,y_test,idx,ax,labels=['MSE','SC','MSE+SC','cGAN+MAE'])
+
