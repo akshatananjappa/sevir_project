f68d15dc0d00e757a55a92f5fdc7727b65063c03
akshatananjappa <91104247+akshatananjappa@users.noreply.github.com>
2022-03-24 19:35:55 -0400

    code updated

--- streamlit_server/web_server/app.py
+++ streamlit_server/web_server/app.py
@@ -18,7 +18,7 @@ API_URL = 'http://2e58-34-74-102-167.ngrok.io/event'
 txt = st.text_input('Location', '')
 
 df_catalog = pd.read_csv('streamlit_server/web_server/catlog_data.csv')
-images_dir = 'images/'
+images_dir = './'
 
 def get_coordinates(location):
     # initialize Nominatim API 
