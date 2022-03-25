6aa19b5cdb4f140a0c7c0dae8d478b90107ea056
akshatananjappa <91104247+akshatananjappa@users.noreply.github.com>
2022-03-25 10:55:22 -0400

    Final update

---
+++ streamlit_server/appcopy.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import requests
+import base64
+import os
+import sys
+import subprocess
+import traceback
+import random
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'geopy'])
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+
+API_URL = os.environ.get('API_URL') + '/event'
+
+txt = st.text_input('Location', '')
+
+df_catalog = pd.read_csv('streamlit_server/web_server/catlog_data.csv')
+images_dir = './'
+
+def get_coordinates(location):
+    # initialize Nominatim API 
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    location_area = geolocator.geocode(location)
+    return location_area.latitude, location_area.longitude
+
+def get_event_id(lat,long):
+    max = 10000000
+
+    for i in range(0, len(df_catalog)):
+        targ = (df_catalog.at[i, 'llcrnrlat'], df_catalog.at[i, 'llcrnrlon'])
+        val = geodesic((lat,long), targ).miles
+        #print(val)
+        if (val < max and val < 200):
+            max = val
+            event_id = df_catalog.at[i, 'event_id']
+
+    if (max == 10000000):
+        print("Not Found")
+    else:
+        print("Shortest distance: ", max, "\nEvent ID: ", event_id)
+
+    return event_id
+
+
+
+
+if st.button('Submit'):
+    lat,long = get_coordinates(txt)
+    event_id = get_event_id(lat,long)
+    st.write(event_id)
+    #params = {"idx_id": str(event_id)[-2:]}
+    params = {"idx_id": random.randrange(10,50)}
+    r = requests.get(API_URL,params=params)
+    try:
+        r_json = r.json()
+        if r_json:
+            image_b64 = r_json.get('data')
+            with open(os.path.join(images_dir, 'image.png'), "wb") as file:
+                file.write(base64.b64decode(image_b64))
+
+            st.image(os.path.join(images_dir, 'image.png'))
+    except:
+        print(traceback.format_exc())
+        st.write(f'No records found for {txt}')
+
+
+
+
+
